98f940b2885a433454eaf566f9f63c5cd0517d33
HeitorContreras <htmcsantos@gmail.com>
2026-05-28 11:01:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 
 st.sidebar.title("Locadora de veículos")
 st.sidebar.image("logo.png")
@@ -8,3 +9,5 @@ valores_diarias = {"Duster":250, "Civic": 300, "Creta": 300, "Amarok": 300, "G63
 
 st.image(f"{carro}.png")
 st.subheader(f"Valor da diária: R$ {valores_diarias[carro]}")
+data_retirada = st.date_input("Selecione a data de retirada: ", datetime.now())
+data_devolucao = st.input("Selecione a data de entrega:", datetime.now())
