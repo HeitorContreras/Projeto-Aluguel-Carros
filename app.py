0d77594c76b946b0fdb9c09d8a53d6cdb728efd9
HeitorContreras <htmcsantos@gmail.com>
2026-05-28 10:29:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.sidebar.title("Locadora de veículos")
 st.sidebar.image("logo.png")
 carro=st.sidebar.selectbox("Selecione o carro que deseja alugar:",
                      ["Duster","Civic","Corolla", "Creta","Amarok","G63"])
-valores_diarias = {"Duster":250, "Civic": 300, "Creta": 300, "Amarok": 300, "G63": 800}
+valores_diarias = {"Duster":250, "Civic": 300, "Creta": 300, "Amarok": 300, "G63": 800, "Corolla": 250}
 
 st.image(f"{carro}.png")
 st.subheader(f"Valor da diária: R$ {valores_diarias[carro]}")
