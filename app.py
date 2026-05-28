76b411a5b986b2b60ce1fbd27b97e151711d64d1
HeitorContreras <htmcsantos@gmail.com>
2026-05-28 09:19:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.sidebar.title("Locadora de veículos")
 st.sidebar.image("logo.png")
-st.sidebar.selectbox("Selecione o carro que deseja alugar:",
+carro=st.sidebar.selectbox("Selecione o carro que deseja alugar:",
                      ["Duster","Civic","Corolla", "Creta","Amarok","G63"])
 st.image(f"{carro}.png", width=750)
