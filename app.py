42e1db3218bba70ed768830ab3a63d5e7f7f68a8
HeitorContreras <htmcsantos@gmail.com>
2026-05-28 10:08:30 -0300

    Include daily rental values in car selection
    Added daily rental values for selected cars.

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ st.sidebar.title("Locadora de veículos")
 st.sidebar.image("logo.png")
 carro=st.sidebar.selectbox("Selecione o carro que deseja alugar:",
                      ["Duster","Civic","Corolla", "Creta","Amarok","G63"])
-st.image(f"{carro}.png", width=750)
+valores_diarias = {"Duster":250, "Civic": 300, "Creta": 300, "Amarok": 300, "G63": 800}
+
+st.image(f"{carro}.png")
+st.subheader(f"Valor da diária: R$ {valores_diarias[carro]}")
